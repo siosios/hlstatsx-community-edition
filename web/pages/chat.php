4ff72b00a524c9b01f72cf6b211d02c85be17846
A1mDev <33463136+A1mDev@users.noreply.github.com>
2026-02-13 04:11:09 +0700

    Fixed a security issue in chat pages

--- web/pages/chat.php
+++ web/pages/chat.php
@@ -1,39 +1,39 @@
 <?php
 /*
-HLstatsX Community Edition - Real-time player and clan rankings and statistics
-Copyleft (L) 2008-20XX Nicholas Hastings (nshastings@gmail.com)
-http://www.hlxcommunity.com
+	HLstatsX Community Edition - Real-time player and clan rankings and statistics
+	Copyleft (L) 2008-20XX Nicholas Hastings (nshastings@gmail.com)
+	http://www.hlxcommunity.com
 
-HLstatsX Community Edition is a continuation of 
-ELstatsNEO - Real-time player and clan rankings and statistics
-Copyleft (L) 2008-20XX Malte Bayer (steam@neo-soft.org)
-http://ovrsized.neo-soft.org/
+	HLstatsX Community Edition is a continuation of 
+	ELstatsNEO - Real-time player and clan rankings and statistics
+	Copyleft (L) 2008-20XX Malte Bayer (steam@neo-soft.org)
+	http://ovrsized.neo-soft.org/
 
-ELstatsNEO is an very improved & enhanced - so called Ultra-Humongus Edition of HLstatsX
-HLstatsX - Real-time player and clan rankings and statistics for Half-Life 2
-http://www.hlstatsx.com/
-Copyright (C) 2005-2007 Tobias Oetzel (Tobi@hlstatsx.com)
+	ELstatsNEO is an very improved & enhanced - so called Ultra-Humongus Edition of HLstatsX
+	HLstatsX - Real-time player and clan rankings and statistics for Half-Life 2
+	http://www.hlstatsx.com/
+	Copyright (C) 2005-2007 Tobias Oetzel (Tobi@hlstatsx.com)
 
-HLstatsX is an enhanced version of HLstats made by Simon Garner
-HLstats - Real-time player and clan rankings and statistics for Half-Life
-http://sourceforge.net/projects/hlstats/
-Copyright (C) 2001  Simon Garner
-            
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+	HLstatsX is an enhanced version of HLstats made by Simon Garner
+	HLstats - Real-time player and clan rankings and statistics for Half-Life
+	http://sourceforge.net/projects/hlstats/
+	Copyright (C) 2001  Simon Garner
+				
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-For support and installation notes visit http://www.hlxcommunity.com
+	For support and installation notes visit http://www.hlxcommunity.com
 */
 
     if (!defined('IN_HLSTATS')) {
@@ -108,6 +108,10 @@ For support and installation notes visit http://www.hlxcommunity.com
 	$serversList = getServersByGame($db, $checkGame);
 	$filter = getChatFilterParam();
 
+	$deleteDaysSafe = isset($g_options['DeleteDays']) ? (int)$g_options['DeleteDays'] : 30;
+	$pageTitle = sprintf('%s %s Server Chat Log (Last %d Days)', eHtml($gamename), eHtml($servername), $deleteDaysSafe);
+	$sectionTitle = printSectionTitle($pageTitle);
+
 	// Functions
 	// Old limit 50
 	function getServersByGame($db, $game, $limit = 0)
@@ -153,7 +157,8 @@ For support and installation notes visit http://www.hlxcommunity.com
 ?>
 
 <div class="block">
-	<?php printSectionTitle("$gamename $servername Server Chat Log (Last ".$g_options['DeleteDays'].' Days)'); ?>
+	<?=$sectionTitle;?>
+	
 	<div class="subblock">
 		<div style="float:left;">
 			<span>
